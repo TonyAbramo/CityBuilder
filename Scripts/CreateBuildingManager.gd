db0e01a1f52eac49c96e72edbf11b40da8f777f4
U-TONY_PC\Tony <anthony.abramo@epitech.eu>
2020-09-19 14:05:21 +0200

    [CreateBuildingaManager] Create grid with every case numbered, 40x40

--- Scripts/CreateBuildingManager.gd
+++ Scripts/CreateBuildingManager.gd
@@ -2,11 +2,18 @@ extends Node
 
 # Liste des buildings
 var building1 = "Buildings/Building.tscn"
+var buildingInfo = "Buildings/BuildingInfo.tscn"
 
+# Tbaleau représentant les 1600 cases de la grille de jeu
+var gridData = [] # 40*40
+
+func _ready():
+	for i in range (1600):
+		gridData.append(0) # Créé un tableau de 1600 0, case vide
 
 func CreateBuilding(pos):
 	# Building sélectionné
-	var building_res = load(building1)
+	var building_res = load(buildingInfo)
 	var building = building_res.instance()
 	building.set_translation(pos)
 	get_tree().get_root().add_child(building)
--- Scripts/HandleMouse.gd
+++ Scripts/HandleMouse.gd
@@ -3,6 +3,8 @@ extends StaticBody
 # Marker case
 var marker
 var positionCreatedBuilding = Vector3()
+var compensateCenterGrid = 20 # Compenser la pos(0, 0) de la grille qui est au centre
+
 
 # Récupération du marker
 func _ready():
@@ -16,8 +18,13 @@ func _on_StaticBody_input_event(camera, event, click_position, click_normal, sha
 	# On place le marker sur la bonne case (survol souris)
 	marker.transform.origin = Vector3(posX, 0, posY)
 
+	# Calcul de la case cliquée
+	var vec = Vector2(round(click_position.x) + compensateCenterGrid, round(click_position.z) + compensateCenterGrid)
+	var posTab = vec.x + (vec.y * 40)
+	print(posTab)
+
 	# Détecter le clic gauche
 	if Input.is_action_just_pressed("clic_gauche"):
 		# Créer le batiment
 		positionCreatedBuilding = Vector3(posX, 0.5, posY)
-		CreateBuildingManager.CreateBuilding(positionCreatedBuilding) 
+		CreateBuildingManager.CreateBuilding(positionCreatedBuilding)
